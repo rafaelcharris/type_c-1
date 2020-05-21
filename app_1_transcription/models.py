1b833059c94aa6f733d23c015669e9c2336cbfa5
Felipe Montealegre <43189041+felmola@users.noreply.github.com>
2020-05-21 15:10:41 -0500

    Added function to check if text is right or wrong. Added fucntion to count the number of right answers

--- app_1_transcription/models.py
+++ app_1_transcription/models.py
@@ -14,14 +14,15 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = 'app_1_transcription'
     players_per_group = None
-    num_rounds = 1
+    num_rounds = 5
     text_list = ["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"]
 
 
 class Subsession(BaseSubsession):
+    # TODO: Have a different text per round
     def creating_session(self):
         for p in self.get_players():
-            p.task_text = Constants.text_list[1]
+            p.task_text = Constants.text_list[0]
 
 
 
@@ -33,8 +34,32 @@ class Player(BasePlayer):
 
     task_text = models.StringField()
     text_input = models.StringField()
+    is_correct = models.IntegerField()
+    accumulated_is_correct = models.IntegerField()
+
+    def check_if_correct(self):
+        if self.task_text == self.text_input:
+            self.is_correct = 1
+        else:
+            self.is_correct = 0
+        print("[[ APP_1_TRANSCRIPTION ]] - PLAYER - check_if_correct().............ROUND NUMBER: ",
+              self.round_number)
+        print("[[ APP_1_TRANSCRIPTION ]] - PLAYER - check_if_correct().............accumulated_is_correct: ",
+              self.is_correct)
+
+    def accumulated_variables(self):
+        if self.round_number == 1:
+            accumulated_is_correct = 0
+        else:
+            self.accumulated_is_correct = sum(filter(None, [p.is_correct for p in self.in_previous_rounds()]))
+        print("[[ APP_1_TRANSCRIPTION ]] - PLAYER - accumulated_variables().............ROUND NUMBER: ",
+              self.round_number)
+        print("[[ APP_1_TRANSCRIPTION ]] - PLAYER - accumulated_variables().............accumulated_is_correct: ",
+              self.accumulated_is_correct)
 
     def report_app_1_transcript(self):
         self.participant.vars['consent_name'] = self.name
-        print("[[ APP_0_CONSENT ]] - PLAYER - CONSENT_ADMIN.............ROUND NUMBER", self.round_number)
-        print("[[ APP_0_CONSENT ]] - PLAYER - CONSENT_ADMIN.............PVARS ARE", self.participant.vars)
+        print("[[ APP_1_TRANSCRIPTION ]] - PLAYER - report_app_1_transcript().............ROUND NUMBER: ",
+              self.round_number)
+        print("[[ APP_1_TRANSCRIPTION ]] - PLAYER - report_app_1_transcript().............PVARS ARE: ",
+              self.participant.vars)
--- app_1_transcription/pages.py
+++ app_1_transcription/pages.py
@@ -13,13 +13,25 @@ class Tarea(Page):
     form_fields = ["text_input"]
 
     def vars_for_template(self):
+        self.player.accumulated_variables()
         return dict(
-            task_text=self.player.task_text
+            task_text=self.player.task_text,
+            is_correct = self.player.is_correct,
+            accumulated_is_correct = self.player.accumulated_is_correct
         )
+
+    def before_next_page(self):
+        self.player.check_if_correct()
 
 
 class Resultados(Page):
-    pass
+
+    form_model = "player"
+
+#    def vars_for_template(self):
+#        return dict(
+#            is_correct=self.player.is_correct
+#        )
 
 page_sequence = [
     Instrucciones,
