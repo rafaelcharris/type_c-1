06176cc5ce84417428ab25ab490583cf08be7310
Felipe Montealegre <43189041+felmola@users.noreply.github.com>
2020-05-24 14:00:18 -0500

    Added bots on app_1_transcription

--- app_1_transcription/tests.py
+++ app_1_transcription/tests.py
@@ -4,3 +4,12 @@ from ._builtin import Bot
 from .models import Constants
 import random, itertools
 
+class PlayerBot(Bot):
+    def play_round(self):
+        if self.round_number == 1:
+            yield(pages.Instrucciones)
+            yield (pages.Tarea, {'text_input': "Text 1"})
+        elif 1 < self.round_number <= Constants.num_rounds:
+            yield(pages.Tarea, {'text_input': "Text 2"})
+            yield (pages.Resultados)
+
