9bd073fd91ede4612f3a4812a11f61e9d40a0e5a
rafaelcharris <rafaelcharris92@gmail.com>
2020-05-28 14:27:55 -0500

    agregar lista de carreras

--- app_10_questionnaire/models.py
+++ app_10_questionnaire/models.py
@@ -71,7 +71,17 @@ class Player(BasePlayer):
                                 choices =["Ciencias Económicas", "Ciencias Agrarias", "Artes","Ciencias","Enfermería",
                                           "Ciencias Humanas", "Derecho, Ciencias Políticas y Sociales", "Ingeniería", "Medicina",
                                           "Odontología", "Medicina Veterinaria y Zootécnia"])
-    carrera = models.StringField(label = '¿En que carrera está usted matriculado?')
+    carrera = models.CharField(label = '¿En que carrera está usted matriculado?',
+                               choices = ["Administración de empresas", "Antropología", "Arquitectura", "Artes plásticas",
+                                          "Biología", "Ciencia Política", "Ciencias de la Computación", "Cine y Televisión",
+                                          "Contaduría Pública", "Derecho", "Diseño Gráfico", "Diseño Industrial", "Economía", "Enfermería",
+                                          "Español y Filolofía clásica", "Estadística", "Estudios Literarios", "Farmacia", "Filología e Idiomas",
+                                          "Filosofía", "Fisioterapia", "Fonoaudiología", "Geografía", "Geología", "Historia", "Ingeniería Agrícola",
+                                          "Ingeniería Agronómica", "Ingeniería Civil", "Ingeniería de Sisstemas y Computación", "Ingeniería Eléctrica",
+                                          "Ingeniería Electrónica", "Ingeniería Industrial", "Ingeniería Mecánica", " Ingeniería Mecatrónica",
+                                          "Ingeniería Química", "Lingüística", "Matemáticas", "Medicina", "medicina Veterinaria", "Música",
+                                          "Música Instrumental", "Nutrición y Dietética", "Odontología", "Psicología", " Química",
+                                          "Sociología", "Teraía Ocupacional", "Trabajo Social", "Zootecnia"])
     veces_matriculado = models.IntegerField(label = "¿Cuantas matrículas ha pagado contando la de este semestre?")
     ed_padre = models.CharField(
         choices=['Ninguno', 'Primaria', 'Bachillerato', 'Algún semestre universitario, pero no graduado', 'Técnico', 'Universitario', 'Posgrado'],
