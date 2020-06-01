ebac2c883fcee4f133942f8bce0fcfb9cffe1623
rafaelcharris <rafaelcharris92@gmail.com>
2020-06-01 10:57:26 -0500

    agregar font y crear imagenes del texto a transcribir

--- to_image.py
+++ to_image.py
@@ -1,23 +1,24 @@
 from PIL import Image, ImageDraw, ImageFont
 
 #fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
-text_list = [ "lah isang anak na lalaki, sa pagkamatay ng isang mapagpasunod at kagalang-galang ama, ay maaaring ma.",
-    "san kanyang, kalungkutan ay una sa lahat, natagpuaan sa isang uri, ng pakikiramay sa kanyang nakaraa.",
-    "yon taong mayaman, ay nagmula sa kanyang kayamanan, rsapagkat naramdaman niya na natural na nakukuha.",
-    "lam mapagpakumbabang, pag-aalaga at masakit na atensiyon na sumasakop, sa mga nasa kanyang kalagayan.",
-    "aks niiwas nila, ang kanilang mga mata sa kanya, o kung ang matinding kalungkutan ng kanyang pagkaba.",
-    "ipi pinipilit silang tumingin sa kanya, ito ay lamang na tumiwalag kaya hindi sumasangayon, sa isang.",
-    "pan kanyang mga aksyon, ay mga bagay ng pangangalaga sa publiko magkalas ng isang salita, mahirap ma.",
-    "pup sa isang malaking, pagpupulong siya ang taong pinagtutuunan ng lahat ng kanilang mga matta, nasa.",
-    "ang kanilang mga, hilig ay tila naghihintay na maghintay, upang matanggap ang kilusan at direkmksyon.",
-    "agk mayroon siya, sa bawat sandali, isang pagkakataon ng mga kagiliw-giliw na sangkatauhan, at sa pa.",
-    "kan yang sarili, ang object ng pagmamasid at kapwalpakiramdam ng bawat katawan, tungkol sa kanya wal."]
+text_list = [ "lah isang anak na lalaki, sa pagkamatay ng isang \n\nmapagpasunod at kagalang-galang ama, ay maaaring ma.",
+    "san kanyang, kalungkutan ay una sa lahat, \n\n natagpuaan sa isang uri, ng pakikiramay sa kanyang nakaraa.",
+    "yon taong mayaman, ay nagmula sa kanyang \n\nkayamanan, rsapagkat naramdaman niya na natural na nakukuha.",
+    "lam mapagpakumbabang, pag-aalaga at masakit na\n\n atensiyon na sumasakop, sa mga nasa kanyang kalagayan.",
+    "aks niiwas nila, ang kanilang mga mata sa kanya,\n\n o kung ang matinding kalungkutan ng kanyang pagkaba.",
+    "ipi pinipilit silang tumingin sa kanya, ito ay\n\n lamang na tumiwalag kaya hindi sumasangayon, sa isang.",
+    "pan kanyang mga aksyon, ay mga bagay ng pangangalaga\n\n sa publiko magkalas ng isang salita, mahirap ma.",
+    "pup sa isang malaking, pagpupulong siya ang taong\n\n pinagtutuunan ng lahat ng kanilang mga matta, nasa.",
+    "ang kanilang mga, hilig ay tila naghihintay na maghintay,\n\n upang matanggap ang kilusan at direkmksyon.",
+    "agk mayroon siya, sa bawat sandali, isang pagkakataon\n\n ng mga kagiliw-giliw na sangkatauhan, at sa pa.",
+    "kan yang sarili, ang object ng pagmamasid at kapwalpakiramdam\n\n ng bawat katawan, tungkol sa kanya wal."]
 
 for text in range(len(text_list)):
-    img = Image.new('RGB', (775, 30), color=(73, 109, 137))
-
+    #https://code-maven.com/create-images-with-python-pil-pillow
+    img = Image.new('RGB', (810, 125), color=(211,211,211))
+    helvetica = ImageFont.truetype(font="Helvetica.ttf", size=30)
     d = ImageDraw.Draw(img)
 
-    d.text((10, 10), text_list[text], fill=(255, 255, 0))
+    d.text((10, 10), text_list[text], font = helvetica, fill=(0,0,0))
 
     img.save("app_1_transcription\\paragraphs\\image" + str(text) +".png")
