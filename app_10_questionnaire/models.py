16b63692816d685413ede9875112ba870629b2f9
Felipe Montealegre <43189041+felmola@users.noreply.github.com>
2020-06-04 17:46:33 -0500

    Standarized questionnaire answers

--- app_10_questionnaire/models.py
+++ app_10_questionnaire/models.py
@@ -28,9 +28,13 @@ def preg_likert(label):
         widget=widgets.Slider,
     )
 def preg_cuatro(label):
-    return models.StringField(
+    return models.IntegerField(
         label = label,
-        choices = ['No', 'Sí, una vez', 'Sí, más de una vez']
+        choices = [
+            (1, 'No'),
+            (2, 'Sí, una vez'),
+            (3, 'Sí, más de una vez')
+        ]
     )
 
 class Player(BasePlayer):
@@ -53,23 +57,32 @@ class Player(BasePlayer):
         if len(str(value)) != 10:
             return "Error: el número de celular debe tener 10 dígitos"
 
-    sexo =models.StringField(
+    sexo =models.IntegerField(
         label= '¿Cuál es su sexo',
-        choices =["Masculino", "Femenino", "Otro"],
+        choices =[
+            (0, "Masculino"),
+            (1, "Femenino"),
+            (2, "Otro")
+    ],
         widget = widgets.RadioSelectHorizontal
     )
     edad = models.PositiveIntegerField(
         label = '¿Cuál es su edad?',
         min = 13, max = 70
     )
-    e_civil = models.CharField(
-        choices=['Soltera/o', 'Casada/o', 'Unión Marital de Hecho (Unión libre)', 'Viuda/o'],
+    e_civil = models.IntegerField(
+        choices=[
+            (1, 'Soltera/o'),
+            (2, 'Unión Marital de Hecho (Unión libre)'),
+            (3, 'Casada/o'),
+            (4, 'Viuda/o')
+        ],
         verbose_name='¿Cuál es su estado civil?',
     )
     facultad = models.CharField(label = '¿ En que facultad está usted matriculado?',
                                 choices =["Ciencias Económicas", "Ciencias Agrarias", "Artes","Ciencias","Enfermería",
                                           "Ciencias Humanas", "Derecho, Ciencias Políticas y Sociales", "Ingeniería", "Medicina",
-                                          "Odontología", "Medicina Veterinaria y Zootécnia"])
+                                          "Odontología", "Medicina Veterinaria y Zootécnia", "Otra"])
     carrera = models.CharField(label = '¿En que carrera está usted matriculado?',
                                choices = ["Administración de empresas", "Antropología", "Arquitectura", "Artes plásticas",
                                           "Biología", "Ciencia Política", "Ciencias de la Computación", "Cine y Televisión",
@@ -80,32 +93,83 @@ class Player(BasePlayer):
                                           "Ingeniería Electrónica", "Ingeniería Industrial", "Ingeniería Mecánica", " Ingeniería Mecatrónica",
                                           "Ingeniería Química", "Lingüística", "Matemáticas", "Medicina", "medicina Veterinaria", "Música",
                                           "Música Instrumental", "Nutrición y Dietética", "Odontología", "Psicología", " Química",
-                                          "Sociología", "Teraía Ocupacional", "Trabajo Social", "Zootecnia"])
+                                          "Sociología", "Terapía Ocupacional", "Trabajo Social", "Zootecnia", "Otra"])
     veces_matriculado = models.IntegerField(label = "¿Cuantas matrículas ha pagado contando la de este semestre?")
-    ed_padre = models.CharField(
-        choices=['Ninguno', 'Primaria', 'Bachillerato', 'Algún semestre universitario, pero no graduado', 'Técnico', 'Universitario', 'Posgrado'],
+    ed_padre = models.IntegerField(
+        choices=[
+            (1, 'Ninguno'),
+            (2, 'Primaria'),
+            (3, 'Bachillerato'),
+            (4, 'Algún semestre universitario, pero no graduado'),
+            (5, 'Técnico'),
+            (6, 'Universitario'),
+            (7, 'Posgrado')
+        ],
         verbose_name='¿Cuál es el nivel de educación de su padre?',
     )
-    ed_madre = models.CharField(
-        choices=['Ninguno', 'Primaria', 'Bachillerato', 'Algún semestre universitario, pero no graduado', 'Técnico', 'Universitario', 'Posgrado'],
+    ed_madre = models.IntegerField(
+        choices=[
+            (1, 'Ninguno'),
+            (2, 'Primaria'),
+            (3, 'Bachillerato'),
+            (4, 'Algún semestre universitario, pero no graduado'),
+            (5, 'Técnico'),
+            (6, 'Universitario'),
+            (7, 'Posgrado')
+        ],
         verbose_name='¿Cuál es el nivel de educación de su madre?',
     )
     estrato = models.IntegerField(
         label = "De acuerdo con las facturas de sus servicios públicos, ¿cuál es el estrato de la vivienda actual donde reside?",
-        choices = [1,2,3,4,5,6],
+        choices = [
+            (1, 'Estrato 1'),
+            (2, 'Estrato 2'),
+            (3, 'Estrato 3'),
+            (4, 'Estrato 4'),
+            (5, 'Estrato 5'),
+            (6, 'Estrato 6'),
+        ],
         widget = widgets.RadioSelectHorizontal
     )
-    ingresos = models.IntegerField( #dar choices?
+    ingresos = models.IntegerField(
+        choices=[
+            (0, 'Menos de 1 salario mínimo'),
+            (1, '1 salarios mínimos'),
+            (2, '2 salarios mínimos'),
+            (3, '3 salarios mínimos'),
+            (4, '4 salarios mínimos'),
+            (5, '5 salarios mínimos'),
+            (6, '6 salarios mínimos'),
+            (7, 'Más de 6 salarios mínimos'),
+        ],
         label = "¿Cuál es el valor aproximado de sus ingresos mensuales en Salarios Mínimos (SMMLV=COP 828,116)? "
                 "(ej. 1 salario mínimo mensual, 2 SMMLV, etc.)"
     )
-    localidad = models.StringField(
-        label = '¿Cuál es la localidad en la que usted reside?',
-        choices = ["No vivo en Bogotá", "Usaquén", "Chapinero", "Santa fé", "La Candelaria",
-                   "Antonio Nariño", "San Cristobal", "Usme", "Sumapaz", "Suba", "Barrios Unidos", "Engativá",
-                   "Teusaquillo", "Fontibón", "Martires", "Puente Aranda", "Kennedy", "Bosa", "Rafael Uribe Uribe", "Tunjuelito",
-                   "Ciudad Bolivar"
-                   ]
+    localidad = models.IntegerField(
+        choices=[
+            (0, 'Fuera de Bogotá'),
+            (1, 'Kennedy'),
+            (2, 'Suba'),
+            (3, 'Engativá'),
+            (4, 'Ciudad Bolívar'),
+            (5, 'Bosa'),
+            (6, 'Usaquén'),
+            (7, 'San Cristobal'),
+            (8, 'Rafael Uribe'),
+            (9, 'Fontibón'),
+            (10, 'Usme'),
+            (11, 'Puente Aranda'),
+            (12, 'Barrios Unidos'),
+            (13, 'Tunjuelito'),
+            (14, 'Teusaquillo'),
+            (15, 'Chapinero'),
+            (16, 'Antonio Nariño'),
+            (17, 'Santa Fe'),
+            (18, 'Los Mártires'),
+            (19, 'La Candelaria'),
+        ],
+        verbose_name='¿En que localidad usted reside?',
+        #        widget=widgets.RadioSelect()
     )
     peso = models.IntegerField(label = "¿Cuál es su peso en kilogramos?")
     altura = models.IntegerField(label = "¿Cuál es su altura en centímetros?",
@@ -115,9 +179,14 @@ class Player(BasePlayer):
                                    '“Totalmente contrario a tomar riesgos”. ')
     riesgo_2 = preg_likert(label = 'Normalmente ¿es usted una persona totalmente dispuesta a tomar riesgos de carácter financiero o intenta evitar tomar riesgos financieros? Por favor conteste usando la siguiente escala de uno a cinco,'
                                    ' donde uno indica “totalmente dispuesto a tomar riesgos” y cinco “Totalmente contrario a tomar riesgos”.')
-    gasto_no_plan = models.StringField(
+    gasto_no_plan = models.IntegerField(
         label = 'Si tuviera que conseguir COP 600.000 en una semana para enfrentar un gasto no planeado, ¿qué tanta dificultad cree que tendría en conseguir el dinero?',
-        choices = ['No tendría dificultad', 'Tendría alguna dificultad, pero lo conseguiría', 'No sé si lo conseguiría', 'Definitivamente no lo conseguiría' ]
+        choices = [
+            (1, 'No tendría dificultad'),
+            (2, 'Tendría alguna dificultad, pero lo conseguiría'),
+            (3, 'No sé si lo conseguiría'),
+            (4, 'Definitivamente no lo conseguiría')
+        ]
     )
     asalto_fisico = preg_cuatro(
         label = '¿Ha sido objeto de asalto físico en los últimos doce meses?'
@@ -159,20 +228,37 @@ class Player(BasePlayer):
     )
     estrato_esperado = models.IntegerField(
         label = '¿Cuál es el estrato de la vivienda donde usted espera vivir a lo largo de su vida?',
-        choices = [1,2,3,4,5,6],
+        choices = [
+            (1, 'Estrato 1'),
+            (2, 'Estrato 2'),
+            (3, 'Estrato 3'),
+            (4, 'Estrato 4'),
+            (5, 'Estrato 5'),
+            (6, 'Estrato 6'),
+        ],
         widget = widgets.RadioSelectHorizontal
     )
 
-    elecciones = models.StringField(
+    elecciones = models.IntegerField(
         label = '¿Con qué frecuencia vota en las elecciones políticas?',
-        choices = ["Todas las veces", "Casi siempre", "Raramente", "Nunca"],
+        choices = [
+            (1, "Nunca"),
+            (2, "Raramente"),
+            (3, "Casi siempre"),
+            (4, "Todas las veces")
+        ],
         widget= widgets.RadioSelectHorizontal
     )
-    botella = models.StringField(
+    botella = models.IntegerField(
         label = 'Imagine la siguiente situación: usted está de compras en una ciudad que no es familiar para usted y se da cuenta de que perdió el camino. Usted decide preguntarle a un extraño por indicaciones. El extraño ofrece llevarlo en su carro al destino que usted tenía. El viaje dura cerca de 20 minutos y le cuesta al extraño 20.000 pesos. El extraño no desea dinero por haberlo llevado. Usted lleva seis botellas de vino con usted. La botella más barata cuesta 5.000 pesos, la botella más cara cuesta 30.000 pesos. '
                 'Usted decide darle una de sus botellas al extraño como agradecimiento por el favor. ¿Cuál botella le daría?', #separar las preguntas para que sea más fácil de leer
-        choices = ['Botella de 5.000 pesos', 'Botella de 10.000 pesos', 'Botella de 15.000 pesos', 'Botella de 20.000 pesos',
-                   'Botella de 25.000 pesos', 'Botella de 30.000 pesos'],
+        choices = [
+            (1, 'Botella de 5.000 pesos'),
+            (2, 'Botella de 10.000 pesos'),
+            (3, 'Botella de 15.000 pesos'),
+            (4, 'Botella de 20.000 pesos'),
+            (5, 'Botella de 25.000 pesos'),
+            (6, 'Botella de 30.000 pesos')],
         widget=widgets.RadioSelect
     )
     self_perception_justicia = models.IntegerField(
