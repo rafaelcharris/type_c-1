1707ad99ba92a873113ee0da0f0e0cf6d8acdc71
rafaelcharris <rafaelcharris92@gmail.com>
2020-06-05 14:37:34 -0500

    agregar bot de numero de telefono

--- app_0_consent/tests.py
+++ app_0_consent/tests.py
@@ -8,8 +8,8 @@ import random, itertools
 class PlayerBot(Bot):
     names = itertools.cycle(['Alberto', 'Bolivar', 'Carlos', 'Danilo', 'Elmer', 'Facundo', 'Guillermo', 'Hernán',
                              'Idilio', 'Janice', 'Kimberly', 'Lola'])
-    phones = itertools.cycle(['1234567891'])
-
+    phones = random.sample(range(100),5)
+    phones = "".join(str(num) for num in phones)
     def play_round(self):
         yield (pages.Verification, {'id_number': random.randint(18, 90), 'e_mail': str(random.randint(1,100)) + "@unal.edu.co" , 'phone': next(
             self.phones)})
